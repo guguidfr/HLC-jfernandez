cdcbbb348cc668507136caeb1299d4cce1145f1c
guguidfr <guguidfr@protonmail.com>
2022-09-28 15:45:26 +0200

    "Cambios de prueba"

--- Lista-de-ejercicios-1/ejercicio10y11.py
+++ Lista-de-ejercicios-1/ejercicio10y11.py
@@ -4,8 +4,10 @@
 def saludo(p1,p2):
         p1=input("Introduce el saludo: ")
         p2=input("Introduce tu nombre: ")
+        
         #print(f"Prueba p1 → {p1}")
         #print(f"Prueba p2 → {p2}")
+        
         #print(f"{p1} {p2}")
         if p1 == "" and p2 == "":
             print("Hello World!")
