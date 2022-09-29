61441c1b1a26d395127f490c387a4fbbc9fd53c4
guguidfr <guguidfr@protonmail.com>
2022-09-29 09:19:43 +0200

    Modificación en las condiciones

--- Lista-de-ejercicios-1/ejercicio14.py
+++ Lista-de-ejercicios-1/ejercicio14.py
@@ -1,7 +1,8 @@
 # José Daniel Fernández López
 # 27-09-2022
 # Da la nota correspondiente al número introducido por el usuario
-nota=int(input("Introduce tu nota: "))
+
+nota=float(input("Introduce tu nota: "))
 if nota < 5 and nota >= 0:
     print("La nota es INSUFICIENTE")
 elif nota >= 5 and nota < 7:
@@ -14,3 +15,20 @@ elif nota == 10:
     print("La nota es MATRÍCULA DE HONOR")
 else:
     print("La nota introducida no es válida")
+
+# Versión con las condiciones más simplificadas
+'''
+nota=float(input("Introduce tu nota: "))
+if 0 <= nota < 5:
+    print("La nota es INSUFICIENTE")
+elif 5 <= nota < 7:
+    print("La nota es SUFICIENTE")
+elif 7 <= nota < 9:
+    print("La nota es NOTABLE")
+elif 9 <= nota < 10:
+    print("La nota es SOBRESALIENTE")
+elif nota == 10:
+    print("La nota es MATRÍCULA DE HONOR")
+else:
+    print("La nota introducida no es válida")
+'''
