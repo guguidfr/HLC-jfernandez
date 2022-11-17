2de5eedf272abf8b4f7ffb950da7ec656e951980
guguidfr <guguidfr@protonmail.com>
2022-11-18 00:35:39 +0100

    Pruebas

--- Apuntes-introduccion/tests_file.py
+++ Apuntes-introduccion/tests_file.py
@@ -1,10 +1,4 @@
-semana = ["Lunes", "Martes", "Miercoles"]
-def return_values():
-    for i in semana:
-        if i == "Miercoles":
-            print("Sí")
-    
-return_values()
-numero = 4
-print(f"El numero es {numero+1}")
-print('a' * 3 + 'e' * 2)
+numero = 5
+cuanto = int(input(f"¿En cuánto quieres aumentar (ej: 5) o reducir (ej: -2) el número? Valor actual: {numero} --> "))
+numero = numero + cuanto
+print(numero)
